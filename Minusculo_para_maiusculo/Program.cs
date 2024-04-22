b61d50294c0577d0087dad39d6b595c874722aaa
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-22 14:51:08 -0300

    Primeiro commit

--- Minusculo_para_maiusculo/Program.cs
+++ Minusculo_para_maiusculo/Program.cs
@@ -1,2 +1,33 @@
 ﻿char[] chars = new char[10];
+char[] resultado = new char[10];
 
+Console.WriteLine("Digite a palavra desejada");
+for (int i = 0; i < 10; i++)
+{
+    chars[i] = char.Parse(Console.ReadLine());
+    switch (chars[i])
+    {
+        case 'a':
+            resultado[i] = 'A';
+            break;
+        case 'i':
+            resultado[i] = 'I';
+            break;
+        case 'e':
+            resultado[i] = 'E';
+            break;
+        case 'o':
+            resultado[i] = 'O';
+            break;
+        case 'u':
+            resultado[i] = 'U';
+            break;
+        default:
+            resultado[i] = chars[i];
+            break;
+    }
+}
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(resultado[i]);
+}
